5932f7831aaabdcc89d9480c3410f91c29c2a052
shahnawazkakarh <shahnawaz.jrw@gmail.com>
2025-09-01 02:53:53 +0500

    calculator module handling

--- python/09-Modules-FileHandling/main.py
+++ python/09-Modules-FileHandling/main.py
@@ -1,4 +1,5 @@
 import calculator as calc
+import string_utlil as str_util
 
 fact_result = calc.factorial(11)
 print(f"Factorial of 5 is: {fact_result}")
@@ -9,6 +10,19 @@ print(f"Sum of 10 and 5 is: {sum_result}")
 square_result = calc.square(11)
 print(f"Square of 11 is: {square_result}")
 
-cube_result = calc.cube(3)
+cube_result = calc.cube(11)
 print(f"Cube of 3 is: {cube_result}")
+
+str_utilresult = str_util.is_palindrome("madam")
+print(f"Is 'madam' a palindrome? {str_utilresult}")
+
+concat_result = str_util.concatenate_strings("Hello, ", "World!")
+print(f"Concatenated string: {concat_result}")
+
+length_result = str_util.string_length("Hello")
+print(f"Length of 'Hello' is: {length_result}")
+
+reverse_result = str_util.reverse_string("Hello")
+print(f"Reversed string of 'Hello' is: {reverse_result}")
+
 
---
+++ python/09-Modules-FileHandling/string_utlil.py
@@ -0,0 +1,15 @@
+## palidome checker
+def is_palindrome(s):
+    return s == s[::-1]
+
+## concatenate two string
+def concatenate_strings(s1, s2):
+    return s1 + s2
+
+## String length
+def string_length(s):
+    return len(s)
+
+## reverse a string
+def reverse_string(s):
+    return s[::-1]
